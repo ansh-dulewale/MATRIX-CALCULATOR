fd6e44ae8b0e4391d3f0f9bebdab80210ada4b6b
ansh67283 <120741334+ansh67283@users.noreply.github.com>
2023-03-17 21:06:41 +0530

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -2,7 +2,7 @@
 int main() {
 	/*n = rows, m = columns*/
     int n, m;
-    char opera; /* opera = operator*/
+    char opera; /*opera = operator*/
     /*Take row and column of the matrix as input from the user*/
     printf("Enter the number of rows and columns: ");
     scanf("%d %d", &n, &m);
@@ -10,15 +10,15 @@ int main() {
 	int A[n][m], B[n][m], C[n][m];
 	/*Take elements of first array as input from the user*/
 	printf("Enter the elements of matrix A:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
             scanf("%d", &A[i][j]);
         }
     }
     /*Take elements of second array as input from the user*/
     printf("Enter the elements of matrix B:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
             scanf("%d", &B[i][j]);
         }
     }
@@ -27,8 +27,8 @@ int main() {
     scanf("%c", &opera);
     /*Addition*/
 	if(opera == '+') {
-		for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+	for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
             C[i][j] = A[i][j] + B[i][j];
             printf("%d ", C[i][j]);
         }
@@ -37,8 +37,8 @@ int main() {
 	}
 	/*Subtraction*/
 	else if(opera == '-') {
-    	for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    	for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
             C[i][j] = A[i][j] - B[i][j];
             printf("%d ", C[i][j]);
         }
@@ -47,10 +47,10 @@ int main() {
 	}
 	/*Multiplication*/
 	else if(opera == '*') {
-		for (int i = 0; i < n; i++) {
+	for(int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             C[i][j] = 0;
-            for (int k = 0; k < m; k++) {
+            for(int k = 0; k < m; k++) {
                 C[i][j] += A[i][k] * B[k][j];
             }
             printf("%d ", C[i][j]);
@@ -60,12 +60,12 @@ int main() {
 	}
 	/*Division*/
 	else if(opera == '/') {
-		for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+		for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
             if (B[i][j] != 0) {
                 C[i][j] = A[i][j] / B[i][j];
             } 
-			else {
+	else {
                 printf("Division by zero error.");
             }
             printf("%d ", C[i][j]);
@@ -79,11 +79,11 @@ int main() {
 	/*Print the final answer*/
 	printf("Answer: "); 
 	{
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++) {
-				printf("%d ",C[i][j]);
-			}
-			printf("\n");
-		}
+	for(int i=0;i<n;i++) {
+	for(int j=0;j<m;j++) {
+	printf("%d ",C[i][j]);
+	}
+	printf("\n");
+	}
 	}
 }
